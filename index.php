9cbac63239d63b5a54ee8d3d7236dfa0594c868e
Jason Hejna <jason.hejna@gmail.com>
2012-09-28 01:16:37 -0400

    good progress with the countdown timer on submit, needs stylin

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['user_id'])) {
 <script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.23.custom.min.js"></script>
 	<!-- jquery for the happiness buttons http://docs.jquery.com/UI/Button#theming -->
-	<script>
+	<script type="text/javascript">
 	var happiness;
 	var unix_time;
 	var date= new Date();
@@ -25,7 +25,7 @@ if (isset($_SESSION['user_id'])) {
 		$("#radio1").click(function(){
     		//$("#boxin").hide();
 			happiness = '1';
-
+			
     		unix_time = date.getTime()/1000;
     		$( "#dialog" ).dialog( "open" );
 			return false;
@@ -69,16 +69,16 @@ if (isset($_SESSION['user_id'])) {
 			modal:true,
 			buttons: {
 						Submit: function() {
+							countDown(80,"status");
 							$( this ).dialog( "close" );
 							//var unix = unix_time;
-
+							$(".background").show();
 							$.ajax({
 							    type: 'post',
 							    url: 'happypost.php',
 							    data: {yahoo:unix_time,happiness:happiness},
-
 							});
-
+							
 						},
 						Cancel: function() {
 							$( this ).dialog( "close" );
@@ -100,7 +100,9 @@ if (isset($_SESSION['user_id'])) {
 </div>
 <div class="grid_18 prefix_3 suffix_3">
 	<br/><br/>
+	<div hidden class="background"><div id="status"></div></div>
 	<div class="boxin">
+		
 	<center><h1 class="q">At this moment, would you say you are</h1>
 	<form>
 	<div id="radio">
@@ -112,6 +114,7 @@ if (isset($_SESSION['user_id'])) {
 	</form>
 	</center>
 	<br/><br/>
+		
 	</div> <!-- end div of boxin css -->
 </div>
 <div class="clear"></div>
@@ -121,6 +124,25 @@ if (isset($_SESSION['user_id'])) {
 </div>
 <div class="clear"></div>
 <div class="grid_24">
+
+ <script type="text/javascript">
+function countDown(secs,elem) {
+var element = document.getElementById(elem);
+element.innerHTML = ""+secs+" seconds remaining until you may answer again";
+if(secs < 1) {
+clearTimeout(timer);
+$("#status").hide();
+$(".background").hide();
+ element.innerHTML = '<h2>Countdown Complete!</h2>';
+ //element.innerHTML += '<a href="#">Click here now</a>';
+ 
+}
+secs--;
+var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+}
+ </script>
+
+
 
 </div>
 </div> <!--where I left 960 end div -->
