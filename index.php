d2eb5308a7cf02d9d1a7aa89a26df8bb5338f9b1
Jason Hejna <jason.hejna@gmail.com>
2012-09-29 06:49:19 -0400

    animated buttons one time yall

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if (isset($_SESSION['user_id'])) {
 <meta charset="utf-8" />
 <title>Happy Data</title>
 <link rel="stylesheet" href="css/angrystyle.css" />
+
 <link rel="stylesheet" href="css/960_24_col.css" />
 <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.23.custom.css" rel="stylesheet" />
 <script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>
@@ -20,6 +21,25 @@ if (isset($_SESSION['user_id'])) {
 	<!-- jquery for the happiness buttons http://docs.jquery.com/UI/Button#theming -->
 	<script type="text/javascript">
 
+		$(function() {
+		$( ".usericons button:first" ).button({
+            icons: {
+                primary: "ui-icon-info"
+            },
+            text: false
+        }).next().button({
+            icons: {
+                primary: "ui-icon-wrench"
+            },
+            text: false
+        }).next().button({
+            icons: {
+                primary: "ui-icon-unlocked"
+            },
+            text: false
+        })
+	});
+	
 	var happiness = 5;
 	var unix_time;
 	var date = new Date();
@@ -133,33 +153,36 @@ if (isset($_SESSION['user_id'])) {
 <body>
 <div class="container_24">
 	<div class="clear"></div>
-<div class="grid_5 prefix_19">
-<div class="myaccount">
-  <a href="mysettings.php">Settings</a>
-  <a href="logout.php">Logout </a>
+<div class="grid_5 prefix_16 suffix_3">
+	<!-- <div class="myaccount">
+	<div id="icons">
+		<a class="ui-icon ui-icon-info" title="info" href="mysettings.php"></a>
+		<a class="ui-icon ui-icon-wrench" title="Account" href="mysettings.php"></a>
+
+		<a class="ui-icon ui-icon-circle-close" title="Logout" href="logout.php"></a>
+	</div>
+	</div> -->
+	<div class="usericons">
+	<button ONCLICK="window.location.href='mysettings.php'">About this site</button>
+	<button ONCLICK="window.location.href='mysettings.php'">Settings</button>
+	<button ONCLICK="window.location.href='logout.php'">Logout</button>
+	</div>
 </div>
-</div>
-<div class="grid_18 prefix_3 suffix_3">
+	<div class="clear"></div>
+	<div class="grid_18 prefix_3 suffix_3">
+
 	<!-- coundown timer time -->
 	 <script type="text/javascript">
-
 	function countDown(secs,elem) {
 	var element = document.getElementById(elem);
 	var mins = Math.round(secs/60);
-	
-	
 	element.innerHTML = ""+mins+"minutes";
-	
-
-	
-
 	if(secs < 1) {
 	clearTimeout(timer);
 	$(".background").hide();
 	$(".boxin").show();
-	 element.innerHTML = '<h2>Countdown Complete!</h2>';
+	 element.innerHTML = 'gogogo';
 	 //element.innerHTML += '<a href="#">Click here now</a>';
-
 	}
 	secs--;
 
@@ -171,28 +194,25 @@ if (isset($_SESSION['user_id'])) {
 			});
 	}
 	 </script>
-	<br/><br/>
+	
 	<div class="clear"></div>
 	<div hidden class="background">
-		
-
+		<center>
 		<table border="0">
 		<tr>
-		<td><div style="width:60px;height:20px;" class="pbartop" id="progressbar"></div></td>
 		<td><h2 class="mini" id="status"></h2></td>
+		<td><div style="width:210px;height:20px;" class="pbartop" id="progressbar"></div></td>
 		</tr>
 		</table> 
-		<center>
-		<form>
-			
-			<h1 hidden class="question0"  id="question0" name="question0">Why do you feel happy?</h1>
-			<h1 hidden class="question1"  id="question1" name="question1">Why do you feel so happy?</h1>
-			<h1 hidden class="question2"  id="question2" name="question2">why aren't you feeling very happy?</h1>
-			<h1 hidden class="question3"  id="question3" name="question3">why aren't you feeling happy?</h1>
+		
+			<h1 hidden class="q"  id="question0" name="question0">Why do you feel so happy?</h1>
+			<h1 hidden class="q"  id="question1" name="question1">Why do you feel happy?</h1>
+			<h1 hidden class="q"  id="question2" name="question2">why aren't you feeling very happy?</h1>
+			<h1 hidden class="q"  id="question3" name="question3">why aren't you feeling happy?</h1>
+			<form>
 			<textarea rows="4" cols="50"></textarea><br/>
-			<input type="submit">
+			<input type="submit" value="Submit">
 		</form>
-
 		<br/><br/>
 		</center>
 	</div>
