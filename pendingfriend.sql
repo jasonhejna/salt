29d11389d1a0d4e9583bc0ecae4ab8417106be00
Jason Hejna <jason.hejna@gmail.com>
2012-10-29 18:36:35 -0400

    added sql

---
+++ pendingfriend.sql
@@ -0,0 +1,8 @@
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (10, 55, 61);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (12, 55, 62);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (13, 57, 62);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (14, 57, 62);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (16, 55, 62);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (17, 57, 62);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (18, 57, 62);
+INSERT INTO `pendingfriend` (`pkey`, `myid`, `friendid`) VALUES (19, 60, 62);
